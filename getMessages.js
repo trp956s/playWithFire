5966c35544e8bca458abab68b8bdf36defecbb81
Springfield Shared Laptop 1 <spr-ml1@asynchrony.com>
2017-10-22 14:58:23 -0500

    000. create basic console firebase application. tp

--- getMessages.js
+++ getMessages.js
@@ -1,22 +1,17 @@
 const firebase = require('firebase');
+const co = require('co');
 
-const config = {
-    apiKey: "AIzaSyDMVqtSYxdqgnH_h9KsCtQxUzQfjEq3yK0",
-    authDomain: "fire-c8b57.firebaseapp.com",
-    databaseURL: "https://fire-c8b57.firebaseio.com",
-    projectId: "fire-c8b57",
-    storageBucket: "fire-c8b57.appspot.com",
-    messagingSenderId: "683165546854"
-};
-
+var config = {see:'add firebase to your web app'};
 firebase.initializeApp(config);
 firebase.database().ref()
     .child('/chats')
-    .child('-Kf9kfiG0rwnlB1gtquA')
-    .once('value').then(function(snapshot) {
-        let message = snapshot.val().content;
-        console.log(message);
+    .on('value', snapshot => {
+        let value = snapshot.val();
+        let contentArray = Object.keys(value).map(key=>value[key].content);
+
+        console.log(Date.now(), contentArray);
     });
 
-for (var i = 0; i < 100000; i++) {
-}
+co(function*(){
+    yield new Promise(resolve => setTimeout(resolve, 1000));
+});
